369862c8831f844c544f184feefec34bd80acb8c
kdtaylor3 <kdtaylor3@crimson.ua.edu>
2020-11-16 18:31:03 -0600

    new

---
+++ pa5-kdtaylor3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pa5_kdtaylor3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
