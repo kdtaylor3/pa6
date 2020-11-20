1ca17be7693442e398cd17ae131c136385e74dd0
kdtaylor3 <kdtaylor3@crimson.ua.edu>
2020-11-20 16:58:15 -0600

    need to add the report that is sorted by year and month

--- pa5-kdtaylor3/Program.cs
+++ pa5-kdtaylor3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace pa5_kdtaylor3
 {
@@ -6,7 +7,96 @@ namespace pa5_kdtaylor3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Welcome to the Audio Book Library";
+            Book[] myBook = new Book[500];
+            Transaction[] myTransaction = new Transaction[500];
+            string userChoice = " ";
+            while (userChoice != "7")
+            {
+                //menu
+                Console.WriteLine("Welcome to this audioBook Library ");
+                Console.WriteLine("Enter 1 to list all of book");
+                Console.WriteLine("Enter 2 to add a book ");
+                Console.WriteLine("Enter 3 to edit Books");
+                Console.WriteLine("Enter 4 to rent a copy of a book");
+                Console.WriteLine("Enter 5 to return a book");
+                Console.WriteLine("Enter 6 to run reports");
+                Console.WriteLine("Enter 7 to exit this program  ");
+                userChoice = Console.ReadLine();
+                Console.Clear();
+
+                //switch
+                switch (userChoice)
+                {
+                    case "1":
+                        Book[] myBooks = ReadIn(myBook);
+
+                        for (int i = 0; i < Book.GetCount(); i++)
+                        {
+                            Console.WriteLine(myBooks[i].ToString());
+                        }
+
+                        break;
+
+                    case "2":
+                        BookUtilities.AddBook(myBook);
+                        break;
+
+                    case "3":
+                        BookUtilities.EditBook(myBook);
+                        break;
+
+                    case "4":
+                        TransactionUtilities.RentBook(myBook, myTransaction);
+                        break;
+
+                    case "5":
+                        TransactionUtilities.ReturnBook(myBook, myTransaction);
+                        break;
+
+                    case "6":
+                        Report.RunReports(myBook, myTransaction);
+                        break;
+
+                    case "7":
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
         }
+        static Book[] ReadIn(Book[] myBook)
+        {
+            Book.SetCount(0);
+            Transaction.SetCount(0);
+            // read file
+            StreamReader inFile = new StreamReader("books.txt");
+
+            string tempFileInput = " ";
+
+            const char delimiter = '#';
+
+            string[] tempArray = new string[6];
+
+            if (File.Exists("books.txt"))
+            {
+                tempFileInput = inFile.ReadLine();
+
+                while (tempFileInput != null)
+                {
+                    tempArray = tempFileInput.Split(delimiter);
+
+                    myBook[Book.GetCount()] = new Book(tempArray[0], tempArray[1], tempArray[2], tempArray[3], int.Parse(tempArray[4]), int.Parse(tempArray[5]));
+
+                    Book.IncCount();
+
+                    tempFileInput = inFile.ReadLine();
+                }
+            }
+            return myBook;
+        }
+
     }
 }
